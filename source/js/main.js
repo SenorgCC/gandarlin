452173e36f8b7caaa9b8a47ff366a024ca686cf2
SenorgCC <alex.sinicyn@gmx.de>
2018-06-22 01:46:34 +0200

    connector test

--- source/js/main.js
+++ source/js/main.js
@@ -1,3 +1,19 @@
 $( document ).ready(function() {
     console.log( "ready!" );
+
+Function psqlconnect(){
+  $.ajax({
+      url:"php/dbconnect.php",
+      //Daten an den Server in JSON
+      //data: {...},
+      //datatype:"json",
+      //Methode Post oder GET
+      type:POST,
+      //callback
+      success: function(data){
+        window.alert(data)
+      }
+  });
+}
+
 });
