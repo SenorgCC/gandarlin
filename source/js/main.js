a25cbcac877c51fd4e6b47a6f0b1b22e92a3943c
SenorgCC <alex.sinicyn@gmx.de>
2018-06-23 00:54:13 +0200

    tabelle

--- source/js/main.js
+++ source/js/main.js
@@ -4,18 +4,34 @@ $( document ).ready(function() {
       alert("HIER!");
       psqlconnect();
     });
-
+function arrayToTable(tableData) {
+        var table = $('<table></table>');
+        $(tableData).each(function (i, rowData) {
+            var row = $('<tr></tr>');
+            $(rowData).each(function (j, cellData) {
+                row.append($('<td>'+cellData+'</td>'));
+            });
+            table.append(row);
+        });
+        return table;
+    }
 function psqlconnect(){
   $.ajax({
       type:'POST',
       url:"source/php/dbconnect.php",
       //Daten an den Server in JSON
       //data: {test:1,a:2},
-      //datatype:"json",
+      datatype:"json",
       //callback
       success: function(data){
-        var antwort=$.parseJSON(data);
-        window.alert(JSON.stringify(data));
+//       var antwort=$.parseJSON(data);
+        var daten = JSON.parse(data);
+        //daten[0]["id"];
+        $('#spieler.basiswerte').append(arrayToTable(daten));
+/*        var basiswerte=[];
+        $.each(daten, function(i,item){
+          basiswerte.push("<tr><td>"+item.kb)
+        })*/
       }
   });
 }
