f1aa2979f11c77ccac76b2e4b47e2da28d28bff5
SenorgCC <alex.sinicyn@gmx.de>
2018-06-22 23:20:38 +0200

    test

--- source/php/dbconnect.php
+++ source/php/dbconnect.php
@@ -1,6 +1,6 @@
 <?php
 ini_set('display_errors', 1);
-$dbconn = pg_connect ("host=localhost port=5432 dbname=gandarlin user=readonly") or die("Konnte keine Verbindung aufbauen");
+$dbconn = pg_connect ("host=localhost port=5432 dbname=gandarlin user=readonly password='readuser'") or die("Konnte keine Verbindung aufbauen");
 $stat = pg_connection_status($dbconn);
 if ($stat === PGSQL_CONNECTION_OK) {
     echo 'Verbindungsstatus ok';
