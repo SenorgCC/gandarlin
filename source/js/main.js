116f67269fdfbbc6e076e355afb9371b95d59e92
SenorgCC <alex.sinicyn@gmx.de>
2018-06-22 02:02:53 +0200

    fix

--- source/js/main.js
+++ source/js/main.js
@@ -5,19 +5,18 @@ $( document ).ready(function() {
       psqlconnect();
     });
 
-});
-
 function psqlconnect(){
   $.ajax({
+      type:POST,
       url:"php/dbconnect.php",
       //Daten an den Server in JSON
-      data: {test:1,a:2},
-      datatype:"json",
+      //data: {test:1,a:2},
+      //datatype:"json",
       //Methode Post oder GET
-      type:POST,
       //callback
       success: function(data){
         window.alert(data)
       }
   });
 }
+});
