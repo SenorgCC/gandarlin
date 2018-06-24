9314f72de60db837efc80cfca31bb66b3fd507cb
SenorgCC <alex.sinicyn@gmx.de>
2018-06-24 09:16:35 +0000

    Datenauszug

--- source/js/main.js
+++ source/js/main.js
@@ -1,31 +1,68 @@
+var basiswert_final ;
+var basiswert_basis ;
+var basiswert_mod ;
+var basiswert_arr= [];
 $( document ).ready(function() {
     console.log( "ready!" );
     $("#myClickBtn").click(function(){
-      alert("HIER!");
-      psqlconnect();
+        bwbasis();
+        bwmod();
+        bwfinal();
     });
 
-
-function psqlconnect(){
+function bwfinal(){
   $.ajax({
       type:'POST',
-      url:"source/php/dbconnect.php",
+      url:"source/php/bwfinal.php",
       //Daten an den Server in JSON
       //data: {test:1,a:2},
       datatype:"json",
       //callback
       success: function(data){
-//       var antwort=$.parseJSON(data);
-        //daten[0]["id"];
-        var $tbody =$('#spielerbasiswerte').find('tbody');
-/*       JSON.parse(data,function(k,b) {
-        $tbody.append('<tr><td>'+k+'</td><td>'+b+'</td><td>'+m+'</td><td>'+f+'</tr>');
-        });
-*/
-        $('#spielerbasiswerte').bootstrapTable({
-            data:JSON.parse(data)
-        });
+      //daten[0]["id"];
+      basiswert_final = JSON.parse(data)[0];
+        basiswert_arr.push(basiswert_final);
       }
   });
 }
+
+function bwbasis(){
+  $.ajax({
+      type:'POST',
+      url:"source/php/bwbasis.php",
+      //Daten an den Server in JSON
+      //data: {test:1,a:2},
+      datatype:"json",
+      //callback
+      success: function(data){
+      //daten[0]["id"];
+      basiswert_basis = JSON.parse(data)[0];
+        basiswert_arr.push(basiswert_basis);
+      }
+  });
+}
+
+function bwmod(){
+  $.ajax({
+      type:'POST',
+      url:"source/php/bwmod.php",
+      //Daten an den Server in JSON
+      //data: {test:1,a:2},
+      datatype:"json",
+      //callback
+      success: function(data){
+      //daten[0]["id"];
+      basiswert_mod = JSON.parse(data)[0];
+        basiswert_arr.push(basiswert_mod);
+      }
+  });
+}
+
 });
+/*       
+      var $tbody =$('#spielerbasiswerte').find('tbody');
+    JSON.parse(data,function(k,b) {
+
+        $tbody.append('<tr><td>'+k+'</td><td>'+b+'</td><td>'+m+'</td><td>'+f+'</tr>');
+        });
+*/
---
+++ source/php/bwmod.php
@@ -0,0 +1,15 @@
+<?php
+include('../../../connect.php');
+ini_set('display_errors', 1);
+//$stmt="select * from basiswerte.basis union all select * from basiswerte.modifikation union all select * from basiswerte.final";
+$stmt="select * from basiswerte.modifikation";
+$result=pg_query($dbconn,$stmt);
+if (!$result){
+    echo "Es ist ein Fehler aufgetreten\n";
+    exit;
+}
+$row=pg_fetch_all($result);
+
+print json_encode($row);
+pg_close($dbconn);
+?>
