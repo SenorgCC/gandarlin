d2e80b2e3ef231ef2666f737ffec756fdc83227d
SenorgCC <alex.sinicyn@gmx.de>
2018-06-26 23:02:21 +0200

    fix

--- source/php/bwbasis.php
+++ source/php/bwbasis.php
@@ -3,8 +3,8 @@ include('../../../connect.php');
 ini_set('display_errors', 1);
 $ID = $_POST['ID'];
 //$stmt="select * from basiswerte.basis union all select * from basiswerte.modifikation union all select * from basiswerte.final";
-$stmt="select * from basiswerte.basis where ID = $1";
-$result = pq_prepare($dbconn,"basiswert",$stmt);
+//$stmt="select * from basiswerte.basis where ID = $1";
+$result = pq_prepare($dbconn,"basiswert",'select * from basiswerte.basis where ID = $1');
 $result=pg_execute($dbconn,"basiswert",array("1"));
 if (!$result){
     echo "Es ist ein Fehler aufgetreten\n";
@@ -12,8 +12,8 @@ if (!$result){
 }
 $row1=pg_fetch_object($result);
 
-$stmt="select * from basiswerte.modifikation where ID = $1";
-$result = pq_prepare($dbconn,"mod",$stmt);
+//$stmt="select * from basiswerte.modifikation where ID = $1";
+$result = pq_prepare($dbconn,"mod",'select * from basiswerte.modifikation where ID = $1');
 $result=pg_execute($dbconn,"mod",array("1"));
 if (!$result){
     echo "Es ist ein Fehler aufgetreten\n";
@@ -22,8 +22,8 @@ if (!$result){
 $result=pg_query($dbconn,$stmt);
 $row2=pg_fetch_object($result);
 
-$stmt="select * from basiswerte.final where ID = $1";
-$result = pq_prepare($dbconn,"final",$stmt);
+//$stmt="select * from basiswerte.final where ID = $1";
+$result = pq_prepare($dbconn,"final",'select * from basiswerte.final where ID = $1');
 $result=pg_execute($dbconn,"final",array("1"));
 if (!$result){
     echo "Es ist ein Fehler aufgetreten\n";
