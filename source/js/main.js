7a10d35fac68424fbcd21db6f780cdd5f06da320
SenorgCC <alex.sinicyn@gmx.de>
2018-07-06 00:15:44 +0200

    fix

--- source/js/main.js
+++ source/js/main.js
@@ -472,15 +472,14 @@ function spielerwaffen(){
         window.alert((tempdata[0]["beschreibung"]));
         for (i=0; i< tempdata.length; i++){
         window.alert(JSON.stringify(tempdata[i]));
-          tempwaffen = [];
-          tempwaffen.push([tempdata[i]["beschreibung"],
+          tempwaffen = [tempdata[i]["beschreibung"],
                         tempdata[i]["schaden_wuerfel"]+"+"+tempdata[i]["schaden"],
                         tempdata[i]["kk_bonus"],
                         tempdata[i]["attackebonus"],
                         tempdata[i]["paradebonus"],
                         tempdata[i]["final_at"],
-                        tempdata[i]["final_pa"]]);
-          spielerwaffen_ar.push(tempwaffen);
+                        tempdata[i]["final_pa"]];
+          spielerwaffen_ar.push.apply(tempwaffen);
         }
       }
     });
