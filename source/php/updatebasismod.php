deb591b0e235ba6e0ad16520677ff87e241fdd6b
SenorgCC <alex.sinicyn@gmx.de>
2018-07-20 01:08:54 +0200

    debug

--- source/php/updatebasismod.php
+++ source/php/updatebasismod.php
@@ -11,16 +11,7 @@ $KK = $_POST['KK'];
 $INT = $_POST['INT'];
 //$stmt="select * from basiswerte.basis union all select * from basiswerte.modifikation union all select * from basiswerte.final";
 //$stmt="select * from basiswerte.basis where ID = $1";
-$result = pg_prepare($dbconn,"basiswert",'UPDATE basiswerte.modifikation
-                                          SET kl = $2,
-                                              gew= $3,
-                                              gsk= $4,
-                                              cha= $5,
-                                              mut= $6,
-                                              kon= $7,
-                                              kk= $8,
-                                              int=$9
-                                          where ID = $1');
+$result = pg_prepare($dbconn,"basiswert",'UPDATE basiswerte.modifikation SET kl = $2, gew= $3, gsk= $4, cha= $5, mut= $6, kon= $7, kk= $8, int= $9 where ID = $1');
 $result=pg_execute($dbconn,"basiswert",array($ID,$KL,$GEW,$GSK,$CHA,$MUT,$KON,$KK,$INT));
 
 print json_encode("OK");
