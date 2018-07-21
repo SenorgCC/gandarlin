740b4a0863f8e0c4b904bd05b4f31ca506cd55b6
SenorgCC <Alex.Sinicyn@gmx.de>
2018-07-21 17:05:52 +0200

    debug

--- source/php/updatebasismod.php
+++ source/php/updatebasismod.php
@@ -10,7 +10,7 @@ $KON = $_POST['KON'];
 $KK = $_POST['KK'];
 $INT = $_POST['INT'];
 $data_ar=array($KL,$GEW,$GSK,$CHA,$MUT,$KON,$KK,$INT,$ID);
-$data=serialize($data_ar);
+$data=$data_ar;
 //$stmt="select * from basiswerte.basis union all select * from basiswerte.modifikation union all select * from basiswerte.final";
 //$stmt="select * from basiswerte.basis where ID = $1";
 $result = pg_prepare($dbconn,"basiswert",'UPDATE basiswerte.modifikation SET kl = $1, gew= $2, gsk= $3, cha= $4, mut= $5, kon= $6, kk= $7, int= $8 where ID = $9');
