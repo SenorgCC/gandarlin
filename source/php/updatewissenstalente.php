051b902b12801d64c52ab27ffc64c7454c852b1b
SenorgCC <alex.sinicyn@gmx.de>
2018-07-27 00:35:36 +0200

    debug

--- source/php/updatewissenstalente.php
+++ source/php/updatewissenstalente.php
@@ -10,7 +10,7 @@ $WEL=$_POST['WEL'];
 $SPR=$_POST['SPR'];
 $ANA=$_POST['ANA'];
 //$data_ar=array($KL,$GEW,$GSK,$CHA,$MUT,$KON,$KK,$INT,$ID);
-$data=array($HEI,$GIF,$SCH,$MAG,$GAS,$WEK,$SPR,$ANA,$ID);
+$data=array($HEI,$GIF,$SCH,$MAG,$GAS,$WEL,$SPR,$ANA,$ID);
 //$stmt="select * from basiswerte.basis union all select * from basiswerte.modifikation union all select * from basiswerte.final";
 //$stmt="select * from basiswerte.basis where ID = $1";
 //$stmt="UPDATE basiswerte.modifikation SET KL = $1, GEW= $2, GSK= $3, CHA= $4, mut= $5, kon= $6, kk= $7, int= $8 where ID = $9";
