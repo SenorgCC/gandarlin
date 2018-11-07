62260c663a908da225eded574c62d1c4ebed6cb8
SenorgCC <alex.sinicyn@gmx.de>
2018-11-08 00:48:16 +0100

    debug

--- source/php/updatespielerruestungen.php
+++ source/php/updatespielerruestungen.php
@@ -6,7 +6,7 @@ include('../../../connect.php');
 //$stmt="UPDATE spieler.ruestung  SET reiten = $1,";
 //for ($i=0; $i < 2 ; $i++) {
   $stmt='UPDATE spieler.ruestung set ausruestungsmodifikation= $1 where name_beschreibung=$2 and id=$3';
-  $data=array(0,"Hakta",1);
+  $data=array(1,'Hakta',1);
   $result = pg_prepare($dbconn,$stmt);
   $result=pg_execute($dbconn,$data);
 //}
