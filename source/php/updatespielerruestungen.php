49772c750839762cfac8bf18afbebfb120ceeec1
SenorgCC <alex.sinicyn@gmx.de>
2018-11-08 00:19:32 +0100

    debug

--- source/php/updatespielerruestungen.php
+++ source/php/updatespielerruestungen.php
@@ -4,8 +4,8 @@ $ID = $_POST['ID'];
 $MOD=$_POST['INPUTARRAY'];
 $Name=$_POST['NAMEARRAY'];
 //$stmt="UPDATE spieler.ruestung  SET reiten = $1,";
-for ($i=0; $i < count($MOD) ; $i++) {
-  $stmt="UPDATE spieler.ruestung set ausruestungsmodifikation= $1 where name_beschreibung=$2 and id=$3;";
+for ($i=0; $i < 2 ; $i++) {
+  $stmt='UPDATE spieler.ruestung set ausruestungsmodifikation= $1 where name_beschreibung=$2 and id=$3;';
   $data=array($MOD[i],$Name[i],$ID);
   $result = pg_prepare($dbconn,"ruestung",$stmt);
   $result=pg_execute($dbconn,"ruestung",$data);
