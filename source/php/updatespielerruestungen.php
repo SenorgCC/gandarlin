2eacd695d958c372ba8df0fccb4acea7a4698c6f
Alex Sinicyn <Alex.Sinicyn@gmx.de>
2018-11-11 12:36:12 +0100

    debug

--- source/php/updatespielerruestungen.php
+++ source/php/updatespielerruestungen.php
@@ -4,8 +4,8 @@ $ID = $_POST['ID'];
 $MOD=$_POST['INPUTARRAY'];
 $Name=$_POST['NAMEARRAY'];
 //$stmt="UPDATE spieler.ruestung  SET reiten = $1,";
-for ($i=0; $i < 2 ; $i++) {
-  $data=array($MOD[$i],(string)$Name[$i],$ID);
+for ($i=0; $i < count($MOD); $i++) {
+  $data=array($MOD[$i],$Name[$i],$ID);
   $stmt="UPDATE spieler.ruestung set ausruestungsmodifikation= $1";
   $stmt.="where name_beschreibung=$2";
   $stmt.="and id=$3";
