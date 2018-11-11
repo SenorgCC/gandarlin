ae6caaaca62cd4dba4075271a67ded328147e42a
Alex Sinicyn <Alex.Sinicyn@gmx.de>
2018-11-11 12:40:17 +0100

    debug

--- source/php/updatespielerruestungen.php
+++ source/php/updatespielerruestungen.php
@@ -4,15 +4,15 @@ $ID = $_POST['ID'];
 $MOD=$_POST['INPUTARRAY'];
 $Name=$_POST['NAMEARRAY'];
 //$stmt="UPDATE spieler.ruestung  SET reiten = $1,";
-for ($i=0; $i < count($MOD); $i++) {
-  $data=array($MOD[$i],$Name[$i],$ID);
+//for ($i=0; $i < count($MOD); $i++) {
+  $data=array($MOD[0],$Name[0],$ID);
   echo ($data);
   $stmt="UPDATE spieler.ruestung set ausruestungsmodifikation= $1";
   $stmt.="where name_beschreibung=$2";
   $stmt.="and id=$3";
-  $result = pg_prepare($dbconn,"test".$i,$stmt);
-  $result=pg_execute($dbconn,"test".$i,$data);
-}
+  $result = pg_prepare($dbconn,"test",$stmt);
+  $result=pg_execute($dbconn,"test",$data);
+//}
 pg_last_error();
 pg_close($dbconn);
 ?>
