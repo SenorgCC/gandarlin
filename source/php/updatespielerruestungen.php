6490d5de730a24f6577c842ab8b5bf521a1f62ae
Alex Sinicyn <Alex.Sinicyn@gmx.de>
2018-11-11 12:15:03 +0100

    test

--- source/php/updatespielerruestungen.php
+++ source/php/updatespielerruestungen.php
@@ -1,18 +1,17 @@
 <?php
 include('../../../connect.php');
-//$ID = $_POST['ID'];
-//$MOD=$_POST['INPUTARRAY'];
-//$Name=$_POST['NAMEARRAY'];
+$ID = $_POST['ID'];
+$MOD=$_POST['INPUTARRAY'];
+$Name=$_POST['NAMEARRAY'];
 //$stmt="UPDATE spieler.ruestung  SET reiten = $1,";
-//for ($i=0; $i < 2 ; $i++) {
-
-  $data=array(42,'Hakta',1);
+for ($i=0; $i < 2 ; $i++) {
+  $data=array($MOD[$i],(string)$Name[$i],$ID);
   $stmt="UPDATE spieler.ruestung set ausruestungsmodifikation= $1";
   $stmt.="where name_beschreibung=$2";
   $stmt.="and id=$3";
-  $result = pg_prepare($dbconn,"test",$stmt);
-  $result=pg_execute($dbconn,"test",$data);
-//}
+  $result = pg_prepare($dbconn,"test".$i,$stmt);
+  $result=pg_execute($dbconn,"test".$i,$data);
+}
 pg_last_error();
 pg_close($dbconn);
 ?>
