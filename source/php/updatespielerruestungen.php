650a47eaa073f22f6415bd96178bf63f18170533
Alex Sinicyn <Alex.Sinicyn@gmx.de>
2018-11-11 12:06:20 +0100

    test

--- source/php/updatespielerruestungen.php
+++ source/php/updatespielerruestungen.php
@@ -5,10 +5,13 @@ include('../../../connect.php');
 //$Name=$_POST['NAMEARRAY'];
 //$stmt="UPDATE spieler.ruestung  SET reiten = $1,";
 //for ($i=0; $i < 2 ; $i++) {
-  $stmt='UPDATE spieler.ruestung set ausruestungsmodifikation= $1 where name_beschreibung=$2 and id=$3';
+
   $data=array(1,'Hakta',1);
-  $result = pg_prepare($dbconn,$stmt);
-  $result=pg_execute($dbconn,$data);
+  $stmt="UPDATE spieler.ruestung set ausruestungsmodifikation= $1";
+  $stmt.="where name_beschreibung=$2";
+  $stmt.="and id=$3";
+  $result = pg_prepare($dbconn,"test",$stmt);
+  $result=pg_execute($dbconn,"test",$data);
 //}
 pg_last_error();
 pg_close($dbconn);
