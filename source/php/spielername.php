dce341b88780363a410342dad025471e1f5329c5
SenorgCC <alex.sinicyn@gmx.de>
2018-11-17 14:31:11 +0100

    test
    :100644 100644 90b4e3c 594d3de M	source/php/spielername.php

--- source/php/spielername.php
+++ source/php/spielername.php
@@ -3,7 +3,7 @@ include('../../../connect.php');
 //$stmt="select * from basiswerte.basis union all select * from basiswerte.modifikation union all select * from basiswerte.final";
 $ID = $_POST['ID'];
 $data=array($ID);
-$stmt="select * from spieler.namen where id = $1;";
+$stmt="select name from spieler.namen where id = $1;";
 $result = pg_prepare($dbconn,"spielernamen",$stmt);
 $result=pg_execute($dbconn,"spielernamen",$data);
 print json_encode(pg_result_error($result));
