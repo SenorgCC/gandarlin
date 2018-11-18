5fef924484f65584d275568c07598480b0418d07
SenorgCC <alex.sinicyn@gmx.de>
2018-11-19 00:00:38 +0100

    debug

--- source/php/spielerwaffen.php
+++ source/php/spielerwaffen.php
@@ -4,8 +4,8 @@ $ID = $_POST['ID'];
 $erg_array = array();
 //$stmt="select * from basiswerte.basis union all select * from basiswerte.modifikation union all select * from basiswerte.final";
 $stmt="select * from spieler.waffen a";
-$stmt.="inner join spieler.waffen_final b on (a.id = b.waffen_id)";
-$stmt.="and b.sp_id = $1')";
+$stmt.=" inner join spieler.waffen_final b on (a.id = b.waffen_id)";
+$stmt.=" and b.sp_id = $1')";
 $result=pg_prepare($dbconn,"normalwerte",$stmt);
 $result=pg_execute($dbconn,"normalwerte",array($ID));
 if (!$result){
