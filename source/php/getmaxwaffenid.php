abc1f5f32868903b9ea7910297dd63e294caa1ed
SenorgCC <alex.sinicyn@gmx.de>
2018-11-24 14:54:46 +0100

    test

--- source/php/getmaxwaffenid.php
+++ source/php/getmaxwaffenid.php
@@ -3,7 +3,9 @@ include('../../../connect.php');
 //$stmt="select * from basiswerte.basis union all select * from basiswerte.modifikation union all select * from basiswerte.final";
 $stmt="select max(id) as id from spieler.waffen";
 $result = pg_query($dbconn,$stmt);
-$name=pg_fetch_object($result);
+while ($row = pg_fetch_row($result)) {
+  $name=$row[0];
+}
 print json_encode($name);
 pg_close($dbconn);
 ?>
