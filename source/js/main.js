f0643c96ac0e834485594c65afb21bbc55e597ae
SenorgCC <alex.sinicyn@gmx.de>
2018-11-24 01:23:38 +0100

    test

--- source/js/main.js
+++ source/js/main.js
@@ -16,7 +16,6 @@ var spielerwaffenkampftalent_arr=[[]];
 var SpielerID;
 var Spielernamen;
 var modalid;
-window.$ = require('jquery');
 window.Popper = require('popper.js');
 
 require('bootstrap');
