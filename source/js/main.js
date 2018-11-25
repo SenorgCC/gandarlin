615a4e23bb48f53739c2a95fb5679acf69ecdf13
Alex Sinicyn <Alex.Sinicyn@gmx.de>
2018-11-25 12:39:41 +0100

    spielerwaffen detect eingebaut

--- source/js/main.js
+++ source/js/main.js
@@ -1389,6 +1389,25 @@ alert("Waffenart:"+waffenart+"Name:"+artname[waffenart]);
 
 });
 // Waffe Modifikation
+$('#spielerwaffen').on('click','tbody td', function(e){
+  var tableData = $(this).children('td').map(function () {
+                    return $(this).text();
+                }).get();
+  var props = $('thead > tr th');
+  var array = [];
+  props.each(function () { array.push($(this).text()) });
+  //keys
+  console.log(array);
+  //values
+  console.log(tableData);
+
+  var obj = {};
+  for (var i = 0; i < tableData.length; i++) {
+      obj[array[i]] = tableData[i];
+  }
+  console.log(obj);
+
+});
  $(document).on('click','#editWaffe',function(event){
    $('#modaleditwaffe').modal("toggle");
 
