724cb3092aa507fb92d3b87bb855af7858692d51
SenorgCC <alex.sinicyn@gmx.de>
2018-11-28 00:23:31 +0100

    test

--- source/js/main.js
+++ source/js/main.js
@@ -1468,7 +1468,7 @@ $('.spielerinventar').on('click','tbody td', function(e){
    var dataobj={};
    var key;
    var value;
-   var attributarr=$('#modtabInventar tr').find('td').map(function(){
+   var attributarr=$('#modtabInventar tr').find('td:first').map(function(){
      key=$(this).text();
      if(key == "Exotisch"){
         value=$(".inventarinput input[type='ratio']:checked").val();
