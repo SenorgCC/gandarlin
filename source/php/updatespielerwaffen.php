ee245b8f8d1c8588efaea8780a60d225c3794dca
SenorgCC <alex.sinicyn@gmx.de>
2018-11-29 02:02:49 +0100

    test

--- source/php/updatespielerwaffen.php
+++ source/php/updatespielerwaffen.php
@@ -13,9 +13,9 @@ $PABONUS=$_POST['PABONUS'];
 $WARTNAME=$_POST['WARTNAME'];
 
 $data=array($BESCH,$WUESCHADEN,$ZSCHADEN,$ATBONUS,$PABONUS,$WP_ID,$SP_ID);
-$stmt="update spieler.waffen set";
-$stmt+=" beschreibung = $1,";
-$stmt+=" schaden_wuerfel = $2,";
+$stmt="UPDATE spieler.waffen set";
+$stmt+=" beschreibung = \'$1\',";
+$stmt+=" schaden_wuerfel = \'$2\',";
 $stmt+=" schaden = $3,";
 $stmt+=" attackebonus = $4,";
 $stmt+=" paradebonus = $5";
@@ -25,10 +25,10 @@ $result=pg_execute($dbconn,"upspwaffe",$data);
 
 $data3=array($WNAME,$WARTNAME,$WAFRT,$WAFEXO,$WP_ID);
 $stmt3="update spieler.waffenart set";
-$stmt3+=" name = $1,";
-$stmt3+=" art = $2,";
+$stmt3+=" name = \'$1\',";
+$stmt3+=" art = \'$2\',";
 $stmt3+=" art_id = $3,";
-$stmt3+=" exotisch = $4";
+$stmt3+=" exotisch = \'$4\'";
 $stmt3+=" where id =$5;";
 $result=pg_prepare($dbconn,"uptspwart",$stmt3);
 $result=pg_execute($dbconn,"uptspwart",$data3);
