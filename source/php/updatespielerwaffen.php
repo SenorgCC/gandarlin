94d2ceedd10c042908d2c46812195faad6593aa3
SenorgCC <alex.sinicyn@gmx.de>
2018-11-29 01:49:39 +0100

    test

--- source/php/updatespielerwaffen.php
+++ source/php/updatespielerwaffen.php
@@ -14,24 +14,24 @@ $WARTNAME=$_POST['WARTNAME'];
 
 $data=array($BESCH,$WUESCHADEN,$ZSCHADEN,$ATBONUS,$PABONUS,$WP_ID,$SP_ID);
 $stmt="update spieler.waffen set";
-$stmt+=" beschreibung=$1,";
-$stmt+=" schaden_wuerfel=$2,";
-$stmt+=" schaden=$3,";
-$stmt+=" attackebonus=$4,";
-$stmt+=" paradebonus=$5";
-$stmt+=" where id=$6 and sp_id=$7;";
-$result=pg_prepare($dbconn,"upspielerwaffe",$stmt);
-$result=pg_execute($dbconn,"upspielerwaffe",$data);
+$stmt+=" beschreibung = $1,";
+$stmt+=" schaden_wuerfel = $2,";
+$stmt+=" schaden = $3,";
+$stmt+=" attackebonus = $4,";
+$stmt+=" paradebonus = $5";
+$stmt+=" where id = $6 and sp_id = $7;";
+$result=pg_prepare($dbconn,"upspwaffe",$stmt);
+$result=pg_execute($dbconn,"upspwaffe",$data);
 
 $data3=array($WNAME,$WARTNAME,$WAFRT,$WAFEXO,$WP_ID);
 $stmt3="update spieler.waffenart set";
-$stmt3+=" name=$1,";
-$stmt3+=" art=$2,";
-$stmt3+=" art_id=$3,";
-$stmt3+=" exotisch=$4";
+$stmt3+=" name = $1,";
+$stmt3+=" art = $2,";
+$stmt3+=" art_id = $3,";
+$stmt3+=" exotisch = $4";
 $stmt3+=" where id =$5;";
-$result=pg_prepare($dbconn,"uptspielerwart",$stmt3);
-$result=pg_execute($dbconn,"uptspielerwart",$data3);
+$result=pg_prepare($dbconn,"uptspwart",$stmt3);
+$result=pg_execute($dbconn,"uptspwart",$data3);
 
 print json_encode(pg_result_error($result));
 pg_close($dbconn);
