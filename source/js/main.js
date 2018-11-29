ae2867b5cbbeb6168bc2c7f147bbbe3f21923bd3
SenorgCC <alex.sinicyn@gmx.de>
2018-11-29 01:44:29 +0100

    test

--- source/js/main.js
+++ source/js/main.js
@@ -1497,7 +1497,7 @@ $('.spielerinventar').on('click','tbody td', function(e){
    wschaden=gesschaden[0];
    //KK-Bonus wird in der DB bereits verrechnet ... Muss abgezogen werden
    zschaden=parseInt(gesschaden[1])-parseInt(spielerwaffen_ar[0][6]);
-   alert(dataobj["id"],dataobj["Name"],dataobj["Art"],artname,dataobj["Exotisch"],dataobj["Beschreibung"]);
+   alert(dataobj["id"]+" "+dataobj["Name"]+" "+dataobj["Art"]+" "+artname+" "+dataobj["Exotisch"]+" "+dataobj["Beschreibung"]);
    $.ajax({
       type:"POST",
       url:"source/php/updatespielerwaffen.php",
