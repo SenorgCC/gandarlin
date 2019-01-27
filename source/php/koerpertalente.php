9a5ca05d2cc1f9c33af1c4b953c9ab8a32809633
Alex Sinicyn <Alex.Sinicyn@gmx.de>
2019-01-27 01:23:15 +0100

    test

--- source/php/koerpertalente.php
+++ source/php/koerpertalente.php
@@ -3,10 +3,7 @@ include('../../../connect.php');
 ini_set('display_errors', 1);
 $ID = $_POST['ID'];
 //$stmt="select * from basiswerte.basis union all select * from basiswerte.modifikation union all select * from basiswerte.final";
-$stmt1="select Schleichen, Aufmerksamkeit, Robustheit, Willenskraft, ";
-$stmt1.="Fingerfertigkeit,Zechen,Taschendiebstahl";
-$stmt1.="from koerpertalente.talentwert where ID = $1";
-$result=pg_prepare($dbconn,"normalwerte",$stmt1);
+$result=pg_prepare($dbconn,"normalwerte",'select * from koerpertalente.talentwert where ID = $1');
 $result=pg_execute($dbconn,"normalwerte",array($ID));
 if (!$result){
     echo "Es ist ein Fehler aufgetreten\n";
@@ -15,11 +12,7 @@ if (!$result){
 $row1=pg_fetch_object($result);
 
 //$stmt="select * from sekundaerwerte.aktuell where ID = $1";
-
-$stmt2="select Schleichen, Aufmerksamkeit, Robustheit, Willenskraft, "
-$stmt2.="Fingerfertigkeit,Zechen,Taschendiebstahl"
-$stmt2.="from koerpertalente.modifikation where ID = $1"
-$result=pg_prepare($dbconn,"mod",$stmt2);
+$result=pg_prepare($dbconn,"mod",'select * from koerpertalente.modifikation where ID = $1');
 $result=pg_execute($dbconn,"mod",array($ID));
 if (!$result){
     echo "Es ist ein Fehler aufgetreten\n";
